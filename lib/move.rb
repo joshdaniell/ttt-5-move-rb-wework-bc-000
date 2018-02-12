4ea53a080cdc3ac6c09e7741809b79dd49dc6ceb
Josh Daniell <josh.daniell@wework.com>
2018-02-12 01:48:28 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,8 +11,5 @@ end
 # code your input_to_index and move method here!
 
 def input_to_index
-  display_board(board)
-  puts "Where would you like to go?"
   move = gets.strip.to.i - 1
-  puts move
 end
